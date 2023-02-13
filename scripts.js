74e289a5f6961da411e2b97e6e782c273819834c
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-02-13 15:32:53 -0500

    Create operate function that calls operation functions

--- scripts.js
+++ scripts.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case 'x':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+        default:
+            console.log("ERROR");
+    }
+}
